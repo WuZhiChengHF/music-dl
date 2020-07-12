298a1287c680b38f53ca179d9045b13071d23986
unknown <936288073@qq.com>
2020-07-12 22:50:15 +0800

    migu2test

---
+++ tests/test_addon_migu2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: HJK
+@file: test_addon_migu2.py
+@time: 2019-06-10
+"""
+
+
+from music_dl.addons import migu2
+
+
+def test_migu2():
+    songs_list = migu2.search("包容")
+    assert songs_list is not None
